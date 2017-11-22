443e3fe13e508ab6720a595d6b4374a2acc62bc4
Compara Online <comparaonline@Comparas-Mac-mini.local>
2017-11-22 12:49:26 -0300

    get listo desde una pagina externa (dolar,euro,uf)

--- ProyectoHernan/ProyectoPostgis/routes/index.js
+++ ProyectoHernan/ProyectoPostgis/routes/index.js
@@ -8,11 +8,11 @@ router.get('/', function(req, res, next) {
 });
 
 const fetch = require("node-fetch");
-const url =
+const dolar_today =
   "http://api.sbif.cl/api-sbifv3/recursos_api/dolar?apikey=2a5a92298fecf09c1472b9e657b5e856709303fe&formato=json";
 
-router.get('/dolar_actual', function(req, res) {
-  fetch(url)
+router.get('/dolar_today', function(req, res) {
+  fetch(dolar_today)
   .then(response => {
     response.json().then(json => {
      
@@ -21,11 +21,24 @@ router.get('/dolar_actual', function(req, res) {
   })
 });
 
-const urlUF =
+const euro_today =
+  "http://api.sbif.cl/api-sbifv3/recursos_api/euro?apikey=2a5a92298fecf09c1472b9e657b5e856709303fe&formato=json";
+
+router.get('/euro_today', function(req, res) {
+  fetch(euro_today)
+  .then(response => {
+    response.json().then(json => {
+     
+      res.json(json);
+    });
+  })
+});
+
+const UF_today =
 "http://api.sbif.cl/api-sbifv3/recursos_api/uf?apikey=2a5a92298fecf09c1472b9e657b5e856709303fe&formato=json";
 
-router.get('/uf_actual', function(req, res) {
-  fetch(urlUF)
+router.get('/uf_today', function(req, res) {
+  fetch(UF_today)
   .then(response => {
     response.json().then(json => {
      
@@ -35,11 +48,11 @@ router.get('/uf_actual', function(req, res) {
 });
 
 
-const urlUF_years =
+const UF_year =
 "http://api.sbif.cl/api-sbifv3/recursos_api/uf/2017?apikey=2a5a92298fecf09c1472b9e657b5e856709303fe&formato=json";
 
 router.get('/uf_2017', function(req, res) {
-  fetch(urlUF_years)
+  fetch(UF_year)
   .then(response => {
     response.json().then(json => {
      
